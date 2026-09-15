8920ce09f17c47adbc82ae2f6b39e44afeea03f2
yash <06yashsharma@gmail.com>
2026-09-15 13:02:20 -0700

    updateconfiguration

--- docker/start.sh
+++ docker/start.sh
@@ -4,6 +4,7 @@ set -e
 
 echo "Clearing Laravel cache..."
 php artisan optimize:clear
+php artisan view:clear
 
 echo "Starting Supervisor..."
-exec /usr/bin/supervisord -n
+exec /usr/bin/supervisord -n -c /etc/supervisor/conf.d/supervisord.conf
